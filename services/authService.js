8748261753cfb69719941b628c14c077ba361d44
Stephanie Cho <love4stephanie@protonmail.com>
2025-02-22 13:24:57 -0500

    sprint5 presnetation

---
+++ services/authService.js
@@ -0,0 +1,20 @@
+import { auth } from './firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+
+export const signUp = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signIn = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
